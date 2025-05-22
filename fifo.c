f7ff840674a559268fdbe8016219136c2425792c
Sushmabt680 <sushmabt.cs23@bmsce.ac.in>
2025-05-22 17:29:20 +0530

    fifo.c

--- fifo.c
+++ fifo.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 int main() {
-    int n, frames, i, j, p, found, index = 0, page_faults = 0, hit = 0;
-char pages[100];
+    int n, frames, i, j, found, index = 0, page_faults = 0, hit = 0;
 
-    printf("Enter the size of the page : ");
+    printf("Enter the size of the page: ");
     scanf("%d", &n);
 
+    char pages[100]; 
     printf("Enter the page string: ");
     scanf("%s", pages);
 
@@ -15,14 +15,16 @@ char pages[100];
 
     int mem[frames];
 
+    
     for (i = 0; i < frames; i++) {
         mem[i] = -1;
     }
 
     for (i = 0; i < n; i++) {
-        int current_page=pages[i]-'0';
+        int current_page = pages[i] - '0';  
         found = 0;
 
+        
         for (j = 0; j < frames; j++) {
             if (mem[j] == current_page) {
                 hit++;
@@ -31,15 +33,16 @@ char pages[100];
             }
         }
 
-
+        
         if (!found) {
-            mem[index] = current_page;;
+            mem[index] = current_page;
             index = (index + 1) % frames;
             page_faults++;
         }
-
-    printf("Page faults: %d \n", page_faults);
+    }printf("Page faults: %d\n", page_faults);
     printf("Page hits: %d\n", hit);
+    printf("Hit Ratio: %d\n", hit);
+    printf("page_fault Ratio: %d\n",page_faults);
 
     return 0;
-    }
+}
