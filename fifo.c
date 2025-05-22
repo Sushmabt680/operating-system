7ef8fe830f1f66dc838b1d76f40f10be20843d25
Sushmabt680 <sushmabt.cs23@bmsce.ac.in>
2025-05-22 16:11:25 +0530

    Add files via upload

---
+++ fifo.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+int main() {
+    int n, frames, i, j, p, found, index = 0, page_faults = 0, hit = 0;
+
+    printf("Enter the size of the page reference string (n): ");
+    scanf("%d", &n);
+
+    char pages[n];
+    printf("Enter the page reference string: ");
+    scanf("%s", pages);
+
+    printf("Enter the number of page frames: ");
+    scanf("%d", &frames);
+
+    int mem[frames];
+
+    for (i = 0; i < frames; i++) {
+        mem[i] = -1;
+    }
+
+    for (i = 0; i < n; i++) {
+        found = 0;
+
+        for (j = 0; j < frames; j++) {
+            if (mem[j] == pages[i]) {
+                hit++;
+                found = 1;
+                break;
+            }
+        }
+
+
+        if (!found) {
+            mem[index] = pages[i];
+            index = (index + 1) % frames;
+            page_faults++;
+    }
+
+    printf("Page faults: %d \n", page_faults);
+    printf("Page hits: %d\n", hit);
+
+    return 0;
+}
+
